0966e4870062dfac77783a651f513d62d7dde938
Nathan Santos <nathangar01@gmail.com>
2021-11-24 08:37:39 -0300

    genre controller

--- src/modules/books/useCases/createGenre/CreateGenreController.ts
+++ src/modules/books/useCases/createGenre/CreateGenreController.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from "express";
-
+import { container } from "tsyringe";
+import { CreateGenreUseCase } from "./CreateGenreUseCase";
 
 class CreateGenreController {
     async handle(request: Request, response: Response): Promise<Response> {
         try{
-            /*
+            
             const { description } = request.body;
 
-            const createGenreUseCase
-            */
+            const createGenreUseCase = container.resolve(
+                CreateGenreUseCase
+            );
+            
+            const genre = await createGenreUseCase.execute({description});
 
-            return response.status(200).json({ message: "irrraaaa"});
+            return response.status(200).json(genre);
 
         }catch (e){
             return response.status(400).json({ error: e.message });
