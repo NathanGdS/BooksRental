450ef9b0ec657807c84675bebd159758efce4011
Nathan Santos <nathangar01@gmail.com>
2021-11-24 08:34:40 -0300

    Create GenresRepository typeorm

---
+++ src/modules/books/infra/typeorm/repositories/GenresRepository.ts
@@ -0,0 +1,36 @@
+import { ICreateGenreDTO } from "@modules/books/dtos/ICreateGenreDTO";
+import { IGenreRepository } from "@modules/books/repositories/IGenreRepository";
+import { getRepository, Repository } from "typeorm";
+import { Genre } from "../entities/Genre";
+
+class GenresRepository implements IGenreRepository {
+
+    private repository: Repository<Genre>;
+
+    public constructor(){
+        this.repository = getRepository(Genre);
+    }
+
+    async create({
+        description
+    }: ICreateGenreDTO): Promise<Genre> {
+        const genre = this.repository.create({description});
+        
+        await this.repository.save(genre);
+
+        return genre;
+    }
+
+    async findGenres(): Promise<Genre[]> {
+        const genres = await this.repository.find();
+        return genres;
+    }
+
+    async findByDescription(description: string): Promise<Genre> {
+        const genre = await this.repository.findOne({description});
+
+        return genre;
+    }
+}
+
+export { GenresRepository };
