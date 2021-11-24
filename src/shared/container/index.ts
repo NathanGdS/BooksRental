9d42e7acf8b03b5bbe19c065c73849ea266ced83
Nathan Santos <nathangar01@gmail.com>
2021-11-24 08:34:55 -0300

    Register Genre Singleton

---
+++ src/shared/container/index.ts
@@ -0,0 +1,8 @@
+import { GenresRepository } from "@modules/books/infra/typeorm/repositories/GenresRepository";
+import { IGenreRepository } from "@modules/books/repositories/IGenreRepository";
+import { container } from "tsyringe";
+
+container.registerSingleton<IGenreRepository>(
+    "GenreRepository",
+    GenresRepository
+);
