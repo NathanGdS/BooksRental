b2bc0b740e3e757d8e22cc584015c4349cd4ed9e
Nathan Santos <nathangar01@gmail.com>
2021-11-24 16:00:42 -0300

    Migration CreateAuthorsBooks

---
+++ src/shared/infra/typeorm/migrations/1637780084381-CreateAuthorsBooks.ts
@@ -0,0 +1,68 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateAuthorsBooks1637780084381 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "authors_books",
+                columns: [
+                    {
+                        name: "author_id",
+                        type: "uuid",
+                        isNullable: false,
+                    },
+
+                    {
+                        name:"book_id",
+                        type: "uuid",
+                        isNullable: false,
+                    },
+
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                ]
+            })
+        );
+
+        await queryRunner.createForeignKey(
+            "authors_books",
+            new TableForeignKey({
+                name: "FKAuthorBook",
+                referencedTableName: "authors",
+                referencedColumnNames: ["id"],
+                columnNames: ["author_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        );
+
+        
+        await queryRunner.createForeignKey(
+            "authors_books",
+            new TableForeignKey({
+                name: "FKABookuthor",
+                referencedTableName: "books",
+                referencedColumnNames: ["id"],
+                columnNames: ["book_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            "authors_bookss",
+            "FKAuthorBook"
+        );
+        await queryRunner.dropForeignKey(
+            "authors_bookss",
+            "FKBookAuthor"
+        );
+        await queryRunner.dropTable("authors_books");
+    }
+}
